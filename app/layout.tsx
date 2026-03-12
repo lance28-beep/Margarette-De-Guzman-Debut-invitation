9c5bbd1820b1720bf1b4eea3716eb29ab34b4e0e
Lance Valle <vallelance8@gmail.com>
2026-03-13 04:18:36 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { siteConfig } from "@/content/site"
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://piel-allen-debut-invitation.weddinginvitationrsvp.com/"
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://margarette-de-guzman-debut-invitation.weddinginvitationrsvp.com/"
 const canonicalUrl = siteUrl.replace(/\/$/, "")
 const desktopHero = "/Details/LinkPreview.jpg"
 const mobileHero = "/Details/LinkPreview.jpg"
