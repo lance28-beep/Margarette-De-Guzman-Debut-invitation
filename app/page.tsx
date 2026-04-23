058170265a6a732de7fc9c09829e33c82ffd0ac2
Rolando Valle <rolandovalle0428@gmail.com>
2026-04-23 19:25:33 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
               <Messages />
               <Details />
               <Entourage />
-              <PrincipalSponsors />
+              {/* <PrincipalSponsors /> */}
               {/* <GuestInformation />
               <WeddingTimeline /> */}
               <GuestList />
